4b27621c1c7fb76a308bed78cac9a6be1b068ab3
Ernandes Ventura <88554072+NANDESVENTURA@users.noreply.github.com>
2023-08-01 10:49:28 -0300

    Update GoogleService.java
    mudança da key exposta do google para secret

--- src/main/java/ApiFrete/api/Service/GoogleService.java
+++ src/main/java/ApiFrete/api/Service/GoogleService.java
@@ -9,9 +9,7 @@ import java.io.IOException;
 
 @Service
 public class GoogleService {
-    public Integer getDistance(DataRegisterShipment dataShipment) throws IOException {
-
-
+     public Integer getDistance(DataRegisterShipment dataShipment,  @Value("${api.service.google.key.secret}") String googleKey ) throws IOException {
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         MediaType mediaType = MediaType.parse("text/plain");
@@ -21,7 +19,7 @@ public class GoogleService {
                 .addQueryParameter("origins", dataShipment.zip_code_origin().toString())
                 .addQueryParameter("destinations", dataShipment.zip_code_destination().toString())
                 .addQueryParameter("units", "metric")
-                .addQueryParameter("key", "AIzaSyCeIkeY1uV0zEXsr_3Lwl_2RKJIcy8YiTM")
+                .addQueryParameter("key", googleKey)
                 .build();
 
         Request request = new Request.Builder()
@@ -35,11 +33,7 @@ public class GoogleService {
         JSONArray arrayRows = jsonObject.getJSONArray("rows");
         JSONArray arrayElements = arrayRows.getJSONObject(0).getJSONArray("elements");
         JSONObject arrayDistance = arrayElements.getJSONObject(0).getJSONObject("distance");
-        Integer value = arrayDistance.getInt("value");
-        System.out.println(arrayRows);
-        System.out.println(arrayElements);
-        System.out.println(arrayDistance);
-        System.out.println(value);
+        Integer value = arrayDistance.getInt("value");        
         return value;
     }
 }
